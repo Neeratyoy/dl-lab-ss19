af5eb14daca56837b1d9fd4652c0cae64bb6517d
Neeratyoy Mallik <mallik@tfpool58.informatik.uni-freiburg.de>
2019-06-05 13:37:16 +0200

    Editing test for history=1

--- exercise3_R/imitation_learning/test.py
+++ exercise3_R/imitation_learning/test.py
@@ -21,8 +21,9 @@ def run_episode(env, agent, rendering=True, max_timesteps=1000, history_length=1
 
     state = rgb2gray(env.reset())
     state = np.array([state])
-    # input_state = np.random.randint(0, 255, (1, history_length, state.shape[-2], state.shape[-1]))
-    # input_state[0][0]
+
+#    if history_length == 1:
+    old_state = state
 
     # fix bug of curropted states without rendering in racingcar gym environment
     env.viewer.window.dispatch_events()
@@ -34,7 +35,7 @@ def run_episode(env, agent, rendering=True, max_timesteps=1000, history_length=1
         #    state = ...
 
 
-        while i < history_length:
+        while history_length > 1 and i < history_length:
             next_state, _, done, _ = env.step(id_to_action(0))
             next_state = rgb2gray(next_state)
             state = np.append(state, [next_state], axis=0)
@@ -49,8 +50,7 @@ def run_episode(env, agent, rendering=True, max_timesteps=1000, history_length=1
         # a = ...
         # when i >= history_length
 
-        # a = id_to_action(3)
-        if np.all(next_state == state):
+        if step > 0 and np.all(old_state == state): # np.all(next_state == state):
             pause = pause + 1
             if pause > 5:
                 push_start = 20
@@ -61,9 +61,11 @@ def run_episode(env, agent, rendering=True, max_timesteps=1000, history_length=1
                     next_state, r, done, info = env.step(a)
                     next_state = rgb2gray(next_state)
                     episode_reward += r
+                    old_state = state
                     state = np.append(state[1:], [next_state], axis=0)
                     step += 1
                 pause = 0
+
         a, _ = agent.predict(np.array([state]))
         # print(a)
         a = np.argmax(a.detach().cpu().numpy()[0])
@@ -71,6 +73,7 @@ def run_episode(env, agent, rendering=True, max_timesteps=1000, history_length=1
         next_state, r, done, info = env.step(a)
         next_state = rgb2gray(next_state)
         episode_reward += r
+        old_state = state
         state = np.append(state[1:], [next_state], axis=0)
         step += 1
 
@@ -90,7 +93,7 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--past', dest='history_length', type=int,
                         help='Number of frames to look back to create one data input.')
     parser.add_argument('-t', '--timesteps', dest='max_timesteps', type=int,
-                        help='Number of yimesteps to run an episode for.')
+                        help='Number of timesteps to run an episode for.')
 
     args = parser.parse_args()
     if args.model is None:
